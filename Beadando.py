23ac6407df6c8051ca61bc8feaa3db4ad5e533d6
gpeti000 <67637657+gpeti000@users.noreply.github.com>
2022-12-30 15:36:28 +0100

    Add files via upload

--- Beadando.py
+++ Beadando.py
@@ -23,17 +23,17 @@ df4 = pd.read_csv("GLD.csv", index_col=0)
 df4.columns = [colname+"_GLD" for colname in df4.columns]
 df5 = pd.read_csv("CORN.csv", index_col=0)
 df5.columns = [colname+"_CORN" for colname in df5.columns]
-df6 = pd.read_csv("yield-curve.csv", index_col=0)
+yield_curve = pd.read_csv("yield-curve.csv", index_col=0)
+
+#adding returns to dataframe
+df1["Daily_return_SPY"] = df1["Adj Close_SPY"] / df1["Adj Close_SPY"].shift(1) - 1
+df2["Daily_return_AGG"] = df2["Adj Close_AGG"] / df2["Adj Close_AGG"].shift(1) - 1
+df3["Daily_return_USO"] = df3["Adj Close_USO"] / df3["Adj Close_USO"].shift(1) - 1
+df4["Daily_return_GLD"] = df4["Adj Close_GLD"] / df4["Adj Close_GLD"].shift(1) - 1
+df5["Daily_return_CORN"] = df5["Adj Close_CORN"] / df5["Adj Close_CORN"].shift(1) - 1
 
 
-#adding log returns to dataframe
-df1["Daily_return_SPY"] = np.log(df1["Adj Close_SPY"] / df1["Adj Close_SPY"].shift(1))
-df2["Daily_return_AGG"] = np.log(df2["Adj Close_AGG"] / df2["Adj Close_AGG"].shift(1))
-df3["Daily_return_USO"] = np.log(df3["Adj Close_USO"] / df3["Adj Close_USO"].shift(1))
-df4["Daily_return_GLD"] = np.log(df4["Adj Close_GLD"] / df4["Adj Close_GLD"].shift(1))
-df5["Daily_return_CORN"] = np.log(df5["Adj Close_CORN"] / df5["Adj Close_CORN"].shift(1))
-
-#gathering log returns into one dataframe
+#gathering returns into one dataframe
 only_returns = pd.DataFrame()
 only_returns["SPY"]=df1["Daily_return_SPY"]
 only_returns["AGG"]=df2["Daily_return_AGG"]
@@ -48,13 +48,14 @@ only_returns = only_returns.loc['2010-06-10':]
 meanReturns = np.mean(only_returns, axis=0)
 covReturns = np.cov(only_returns, rowvar=False)
 
-# USD Govt yield curve 3 month
-RiskFree = np.log(df6["close"]/df6["close"].shift(1))
-RiskFree = RiskFree.loc['2010-06-10':]
-RiskFreeMean = np.mean(RiskFree)/252
 
-def minimize_this(weights, meanReturns, covReturns, RiskFreeMean):
-    excess_return = np.matmul(np.array(meanReturns), weights.transpose()) - RiskFreeMean
+# USD Govt yield curve 3 month
+RiskFree = yield_curve["close"]
+RiskFree = RiskFree.loc['2010-06-10':]
+RiskFreeMeanDaily = np.mean(RiskFree)/252
+
+def minimize_this(weights, meanReturns, covReturns, RiskFreeMeanDaily):
+    excess_return = np.matmul(np.array(meanReturns), weights.transpose()) - RiskFreeMeanDaily
     stand_dev = np.sqrt(np.matmul(np.matmul(weights, covReturns), weights.transpose()))
     negative_sharpe = -(excess_return / stand_dev)
     return negative_sharpe
@@ -71,8 +72,45 @@ cons = ({'type': 'eq', 'fun': constraint})
 for i in range(5):
     bounds.append((0,1))
 
-optimization = optimize.minimize(minimize_this, x0=x0, args=(meanReturns, covReturns, RiskFreeMean), method='SLSQP', bounds=bounds, constraints=cons, tol=10 ** -3)
+optimization = optimize.minimize(minimize_this, x0=x0, args=(meanReturns, covReturns, RiskFreeMeanDaily), method='SLSQP', bounds=bounds, constraints=cons, tol=10 ** -3)
 print(optimization.fun*np.sqrt(252))
 print(optimization.x)
+
+def csuszo_ablak(only_returns, yield_curve, start_date, end_date):
+
+    only_returns = only_returns.loc[start_date:end_date]
+    yield_curve = yield_curve.loc[start_date:end_date]
+
+    meanReturns = np.mean(only_returns, axis=0)
+    meanYC = np.mean(yield_curve, axis=0)
+    covReturns = np.cov(only_returns, rowvar=False)
+
+    RF_daily = meanYC / 252
+
+    def minimize_this_cs(weights, meanReturns, covReturns, RF_daily):
+        excess_return = np.matmul(np.array(meanReturns), weights.transpose()) - RF_daily
+        stand_dev = np.sqrt(np.matmul(np.matmul(weights, covReturns), weights.transpose()))
+        negative_sharpe = -(excess_return / stand_dev)
+        return negative_sharpe
+
+    def constraint_cs(weights):
+        sum = 0
+        for weight in weights:
+            sum = sum + weight
+        return sum - 1
+
+    bounds = []
+    x0 = np.array([0.2, 0.2, 0.2, 0.2, 0.2])
+    cons = ({'type': 'eq', 'fun': constraint})
+    for i in range(5):
+        bounds.append((0, 1))
+
+    optimization = optimize.minimize(minimize_this, x0=x0, args=(meanReturns, covReturns, RF_daily), method='SLSQP',
+                                     bounds=bounds, constraints=cons, tol=10 ** -3)
+    return [optimization.fun, optimization.x]
+
+dates=only_returns.index
+#print(dates[1258:])
+
 
 pass
